e769a94353713f10ca8ae89d6ba300a32365c6f4
so00yeon <96031597+so00yeon@users.noreply.github.com>
2021-12-23 11:56:04 +0900

    #5_templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,34 @@
-export const trending = (req, res) => res.send("Home page Videos");
-export const see = (req, res) => {
-    return res.send(`Watch #${req.params.id}`);
+export const trending = (req, res) => {
+    const videos = [
+        { 
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2minutes ago",
+            views: 59,
+            id: 1,
+        },
+        { 
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2minutes ago",
+            views: 59,
+            id: 1,
+        },
+        { 
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2minutes ago",
+            views: 59,
+            id: 1,
+        },
+    ];
+    return res.render("home", {pageTtle: "Home", videos});
 };
-export const edit = (req, res) => {
-    return res.send(`Edit #${req.params.id}`);
-};
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit", {pageTitle:"Edit"});
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
